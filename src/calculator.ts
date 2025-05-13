cdbfbd4f8a1a64d147c34bb8ad1604210f8d1cf4
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2025-05-13 23:14:30 +0530

    refactor: remove condition for 1 number

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,8 +2,6 @@ export function add(numbers:string){
     
     if(!numbers) return 0
 
-    if (numbers.length === 1) return parseInt(numbers)
-
     const inputs = numbers.split(',')
 
     return inputs.map(number => parseInt(number)).reduce((a, b) => a + b, 0)
