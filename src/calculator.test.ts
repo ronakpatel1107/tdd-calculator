b71cdf4cf359d61859c22ef3ab868b3f5146bac9
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2025-05-13 23:18:37 +0530

    test:  allow new lines between numbers

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -2,30 +2,36 @@ import { add } from './calculator';
 
 describe('Calculator', () => {
   test('should returns 0 for empty string', () => {
-    expect(add('')).toBe(0); // Fails: add() doesn't exist yet
+    expect(add('')).toBe(0); 
   });
 });
 
 describe('Calculator', () => {
   test('should return the number itself for a single number input', () => {
-    expect(add('1')).toBe(1); // Fails: add() doesn't exist yet
+    expect(add('1')).toBe(1); 
   });
 });
 
 describe('Calculator', () => {
   test('should return sum of two numbers for two input', () => {
-    expect(add('2,4')).toBe(6); // Fails: add() doesn't exist yet
+    expect(add('2,4')).toBe(6); 
   });
 });
 
 describe('Calculator', () => {
   test('should return sum of all numbers for multiple inputs', () => {
-    expect(add('2,4,1')).toBe(7); // Fails: add() doesn't exist yet
+    expect(add('2,4,1')).toBe(7); 
   });
 });
 
 describe('Calculator', () => {
   test('should return sum of all numbers for multiple inputs', () => {
-    expect(add('2,4,1,4,5')).toBe(16); // Fails: add() doesn't exist yet
+    expect(add('2,4,1,4,5')).toBe(16); 
+  });
+});
+
+describe('Calculator', () => {
+  test('should return 5', () => {
+    expect(add('2,\n3')).toBe(5); 
   });
 });
