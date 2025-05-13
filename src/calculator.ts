821ddcc3aab016909373a7b8d3a4f77e890aa674
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2025-05-13 23:07:49 +0530

    feat: return sum of two numbers for two input

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,6 +2,10 @@ export function add(numbers:string){
     
     if(!numbers) return 0
 
-    return parseInt(numbers)
-    
+    if (numbers.length === 1) return parseInt(numbers)
+
+    const inputs = numbers.split(',')
+    console.log(inputs)
+    return parseInt(inputs[0])+ parseInt(inputs[1]
+)    
 }
