7acff15fd1b723447b433f43b352b7ed5ba4fef8
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2025-05-14 08:22:24 +0530

    feat : allow custom delimiter and perform sum

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,6 +2,10 @@ export function add(numbers:string){
     
     if(!numbers) return 0
 
+    if(numbers.startsWith('//')){
+       numbers = numbers.substring(4).replace(';',',') 
+    }
+
     const inputs = numbers.replace(/\n/g,',').split(',')
 
     return inputs.map(number => parseInt(number)).reduce((a, b) => a + b, 0)
